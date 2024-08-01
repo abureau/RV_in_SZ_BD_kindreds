6df305e50a83627b287121365cd2211046896ed7
lmangnier <loic.mangnier@crchudequebec.ulaval.ca>
2024-08-01 10:28:11 -0400

    add code to integrate hg38 genebody to CRH file

--- Mangnier2024/code/code_post_analysis_signal.R
+++ Mangnier2024/code/code_post_analysis_signal.R
@@ -1,7 +1,14 @@
+#This code allows the production of post analyses in the real-data application on SZ BP pedigrees 
+#Paths and directories have to be changed, all data for lifting over are present in the repo
+
+#Functions from CRHs.R have to be imported 
+source("CRHs.R")
+
 load("C:\\Users\\loicm\\Documents\\CRHs_SZ\\data\\grandtab.RData")
 
 
-ABC_Pred = process.ABC("C:\\Users\\loicm\\Documents\\CRHs_SZ\\data\\EnhancerPredictions_NEU_iPSC.txt")
+
+ABC_Pred = process.ABC("C:\\Users\\loicm\\Documents\\CRHs_SZ\\data\\EnhancerPredictions_NEU_iPSC.txt",genebody = T)
 TADs_DI_iPSC_NEU = rtracklayer::import("C:\\Users\\loicm\\Documents\\CRHs_SZ\\data\\TADs_DI_iPSC_NEU.bed", format = "bed")
 
 #Liftover des Enhancers et Gènes de hg19 vers hg38
@@ -13,8 +20,7 @@ colnames(liftover_hg38_TADs) = c("chrom_on_hg38","start_hg38","end_hg38","origin
 liftover_hg38_TADs$chrom_on_hg19 = sapply(strsplit(liftover_hg38_TADs$original_TAD_hg19, ":", fixed=TRUE), function(x) x[1])
 
 sum(liftover_hg38_TADs$chrom_on_hg38 != liftover_hg38_TADs$chrom_on_hg19)
-#Le nombre de TADs pour lesquels le liftover a donné des régions situées sur d'autres chromosomes
-#Je retire donc ces régions 
+#We remove regions located on other chromosomes
 
 liftover_hg38_TADs = liftover_hg38_TADs[liftover_hg38_TADs$chrom_on_hg38 == liftover_hg38_TADs$chrom_on_hg19,]
 GRanges_TADs_liftover_hg38 = GRanges(seqnames = liftover_hg38_TADs$chrom_on_hg38 ,ranges=IRanges(start=liftover_hg38_TADs$start_hg38,end=liftover_hg38_TADs$end_hg38))
@@ -38,23 +44,36 @@ liftover_hg38_Genes$TargetGene = sapply(strsplit(liftover_hg38_Genes$original_Ge
 liftover_hg38_Genes = liftover_hg38_Genes[liftover_hg38_Genes$chrom_on_hg19==liftover_hg38_Genes$chrom_on_hg38,]
 GRanges_Genes_liftover_hg38 = GRanges(seqnames = liftover_hg38_Genes$chrom_on_hg38 ,ranges=IRanges(start=liftover_hg38_Genes$start_hg38,end=liftover_hg38_Genes$end_hg38), TargetGene=liftover_hg38_Genes$TargetGene)
 
+
+liftover_hg38_GenesBody = read.table("C:\\Users\\loicm\\Downloads\\hglft_genome_12321_7a2b30.bed", header=FALSE)
+colnames(liftover_hg38_GenesBody) = c("chrom_on_hg38","start_hg38","end_hg38","TargetGene","n_correspondance", "strand")
+
+GRanges_GenesBody_liftover_hg38 = GRanges(seqnames = liftover_hg38_GenesBody$chrom_on_hg38 ,ranges=IRanges(start=liftover_hg38_GenesBody$start_hg38,end=liftover_hg38_GenesBody$end_hg38), TargetGene=liftover_hg38_GenesBody$TargetGene)
+
 df_Enhancers.hg38.final = as.data.frame(GRanges_Enhancers_liftover_hg38)
 colnames(df_Enhancers.hg38.final) = c("chr.x","start.hg38","end.hg38","widthEnh.hg38","strand","name")
 df_Genes.hg38.final = as.data.frame(GRanges_Genes_liftover_hg38)
 colnames(df_Genes.hg38.final) = c("chr.x","startProm.hg38","endProm.hg38","widthProm.hg38","strand","TargetGene")
+df_GenesBody.hg38.final = as.data.frame(GRanges_GenesBody_liftover_hg38)
+colnames(df_GenesBody.hg38.final) = c("chr.x","startGene.hg38","endGene.hg38","widthGene.hg38","strand","TargetGene")
 
-ABC_Pred.for.liftover = merge(merge(ABC_Pred.for.liftover, df_Enhancers.hg38.final[,c("chr.x","start.hg38","end.hg38", "name")],by=c("chr.x", "name"), all.x=TRUE), df_Genes.hg38.final[,c("chr.x","startProm.hg38","endProm.hg38","TargetGene")], by=c("chr.x","TargetGene"),all.x=TRUE)
+#Set commons levels for chr.x avoiding weird behaviors of the merge function
+ABC_Pred.for.liftover$chr.x = factor(ABC_Pred.for.liftover$chr.x, levels=paste0("chr",1:22))
+df_GenesBody.hg38.final$chr.x = factor(df_GenesBody.hg38.final$chr.x, levels = paste0("chr",1:22))
+df_Enhancers.hg38.final$chr.x = factor(df_Enhancers.hg38.final$chr.x, levels=paste0("chr",1:22))
+df_Genes.hg38.final$chr.x = factor(df_Genes.hg38.final$chr.x, levels=paste0("chr",1:22))
+
+ABC_Pred.for.liftover = merge(merge(merge(ABC_Pred.for.liftover, df_Enhancers.hg38.final[,c("chr.x","start.hg38","end.hg38", "name")], by=c("chr.x", "name"), all.x=TRUE), df_Genes.hg38.final[,c("chr.x","startProm.hg38","endProm.hg38","TargetGene")], by=c("chr.x","TargetGene"), all.x=TRUE), na.omit(df_GenesBody.hg38.final[!duplicated(df_GenesBody.hg38.final[, c("chr.x","TargetGene")]), c("chr.x", "startGene.hg38", "endGene.hg38", "TargetGene")]), by=c("chr.x","TargetGene"), all.x=T)
 ABC_Pred.for.liftover.tmp = ABC_Pred.for.liftover
-ABC_Pred.for.liftover = ABC_Pred.for.liftover[,c("chr.x","start.hg38","end.hg38", "name","startProm.hg38","endProm.hg38","TargetGene","TargetGeneTSS", "CellType","ABC.Score",     "typeOf", "chr.y")]
-colnames(ABC_Pred.for.liftover)=c("chr.x","start", "end", "name", "startProm","endProm", "TargetGene","TargetGeneTSS", "CellType","ABC.Score",     "typeOf", "chr.y")
+ABC_Pred.for.liftover = ABC_Pred.for.liftover[,c("chr.x","start.hg38","end.hg38", "name","startProm.hg38","endProm.hg38","startGene.hg38","endGene.hg38","TargetGene","TargetGeneTSS", "CellType","ABC.Score",     "typeOf", "chr.y")]
+colnames(ABC_Pred.for.liftover)=c("chr.x","start", "end", "name", "startProm","endProm","startGeneBody","endGeneBody", "TargetGene","TargetGeneTSS", "CellType","ABC.Score",     "typeOf", "chr.y")
 
 ABC_Pred.for.liftover$name = paste0(ABC_Pred.for.liftover$start,"-" ,ABC_Pred.for.liftover$end)
 CRHs_iPSC_NEU.liftover.hg38=create.CRHs(ABC_Pred.for.liftover, "ABC")
 
 ABC_Pred_with_membership.liftover.hg38 = add.membership(CRHs_iPSC_NEU.liftover.hg38,ABC_Pred.for.liftover)
 
-write.table(ABC_Pred_with_membership.liftover.hg38, "C:\\Users\\loicm\\Documents\\CRHs_SZ\\EnhancerPredictions_NEU_iPSC_liftoverhg38.txt", col.names = TRUE, row.names = TRUE, sep="\t", quote=F)
-
+write.table(ABC_Pred_with_membership.liftover.hg38, "C:\\Users\\loicm\\Documents\\CRHs_SZ\\EnhancerPredictions_NEU_iPSC_liftoverhg38_withGeneBody.txt", col.names = TRUE, row.names = TRUE, sep="\t", quote=F)
 
 po = coverage.By.CRH(CRHs_iPSC_NEU.liftover.hg38, ABC_Pred_with_membership.liftover.hg38, method="ABC")
 
@@ -89,5 +108,5 @@ V(CRHs_iPSC_NEU.liftover.hg38)$Name = names(V(CRHs_iPSC_NEU.liftover.hg38))
 
 decompose_CRHs_iPSC_NEU.liftover.hg38 = decompose(CRHs_iPSC_NEU.liftover.hg38)
 
-ggnet2(decompose_CRHs_iPSC_NEU.liftover.hg38[[1438]], size=4,color="Elements", shape="Elements", palette = "Set1",label="Name",label.size = 3,legend.size = 20,
+GGally::ggnet2(decompose_CRHs_iPSC_NEU.liftover.hg38[[1438]], size=4,color="Elements", shape="Elements", palette = "Set1",label="Name",label.size = 3,legend.size = 20,
        legend.position = "none")
