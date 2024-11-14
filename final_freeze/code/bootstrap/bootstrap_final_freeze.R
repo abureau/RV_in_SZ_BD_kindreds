82083eba5f35d22399916376972d621a1b13f70d
abureau <abureau6@gmail.com>
2024-11-14 13:36:09 -0500

    Update bootstrap_final_freeze.R

--- final_freeze/code/bootstrap/bootstrap_final_freeze.R
+++ final_freeze/code/bootstrap/bootstrap_final_freeze.R
@@ -31,6 +31,7 @@ mean(boot.TAD.chr21.1.orig.ACAT<=results$ACAT)
 system.time(bootRetroFunRVS(TAD.chr21.1.orig$agg.genos.by.fam, n.unique.config.by.fam=null$N, Z_annot = TAD.chr21.1.orig$Z_annot[,-4], prob.sharing.by.fam=null$pattern.prob,nullval=exp_var,nrep=1))
 system.time(resample.genos.by.fam(agg.genos.by.fam = TAD.chr21.1.orig$agg.genos.by.fam, n.unique.config.by.fam=null$N, prob.sharing.by.fam=null$pattern.prob))
 
+set.seed(10)
 CRH998.agg = extract_CRH(TAD.chr21.1.orig,"CRH998")
 CRH998.boot = bootRetroFunRVS(CRH998.agg, n.unique.config.by.fam=null$N, prob.sharing.by.fam=null$pattern.prob,nullval=exp_var,nrep=10000)
 
@@ -43,8 +44,29 @@ mean(CRH998.boot>=score_orig.CRH998)
 CRH1006.agg = extract_CRH(TAD.chr21.1.orig,"CRH1006")
 
 # Avec relations cryptiques
-TAD.chr20.1.cryptic = agg.genos.annotations.TAD(pheno="GCbr", with_exons=TRUE, consanguinity=TRUE, path_retrofun=path_retrofun, chr=20, TAD=1, maxvar = 300)
+TAD.chr21.1.cryptic = agg.genos.annotations.TAD(pheno="GCbr", with_exons=TRUE, consanguinity=TRUE, path_retrofun=path_retrofun, chr=21, TAD=1, maxvar = 300)
+null_name <- "consanguinity.cryptique.seq.prob.dist.rds"
+null <- readRDS(paste0(path_retrofun, "/objets_ped/", null_name))
 exp_var_cryptic_name <- "expected.variance.consanguinity.cryptique.seq.rds"
-exp_var_cryptic <- readRDS(paste0(path_retrofun, "/objets_ped/", exp_var_cryptic_name))
+exp_var.cryptic <- readRDS(paste0(path_retrofun, "/objets_ped/", exp_var_cryptic_name))
 
-results <- RetroFun.RVS(exp_var_cryptic, TAD.chr20.1.cryptic$agg.genos.by.fam, Z_annot = TAD.chr20.1.cryptic$Z_annot, W = rep(1, nrow(TAD.chr20.1.cryptic$Z_annot)), independence=FALSE)
+results <- RetroFun.RVS(exp_var_cryptic, TAD.chr21.1.cryptic$agg.genos.by.fam, Z_annot = TAD.chr21.1.cryptic$Z_annot, W = rep(1, nrow(TAD.chr21.1.cryptic$Z_annot)), independence=FALSE)
+
+set.seed(10)
+boot.TAD.chr21.1.cryptic = bootRetroFunRVS(TAD.chr21.1.cryptic$agg.genos.by.fam, n.unique.config.by.fam=null$N, Z_annot = TAD.chr21.1.cryptic$Z_annot[,-4], prob.sharing.by.fam=null$pattern.prob,nullval=exp_var,nrep=1000)
+Var.Stat = unlist(compute.Var.by.Annot(exp_var,TAD.chr21.1.cryptic$agg.genos.by.fam,TAD.chr21.1.cryptic$Z_annot,W= rep(1, nrow(TAD.chr21.1.cryptic$Z_annot)),independence = FALSE))
+
+pmat = pnorm(boot.TAD.chr21.1.cryptic/matrix(sqrt(Var.Stat[-4]),1000,3,byrow = T),0,1,lower.tail = FALSE)
+
+boot.TAD.chr21.1.cryptic.ACAT = apply(pmat,1,function(x) ACAT::ACAT(x[!is.nan(x)]))
+mean(boot.TAD.chr21.1.cryptic.ACAT<=results$ACAT)
+# [1] 0.008
+
+set.seed(10)
+CRH998.cryptic.agg = extract_CRH(TAD.chr21.1.cryptic,"CRH998")
+CRH998.cryptic.boot = bootRetroFunRVS(CRH998.cryptic.agg, n.unique.config.by.fam=null$N, prob.sharing.by.fam=null$pattern.prob,nullval=exp_var.cryptic,nrep=10000)
+
+annotation.matrix = matrix(1, ncol=1, nrow=max(CRH998.agg$index_variants))
+score_cryptic.CRH998 = compute.Burden.by.Annot(exp_var, CRH998.cryptic.agg,  Z_annot = annotation.matrix, W = rep(1, nrow(annotation.matrix)))$B[1,1]
+mean(CRH998.cryptic.boot>=score_cryptic.CRH998)
+#[1] 0.0019
