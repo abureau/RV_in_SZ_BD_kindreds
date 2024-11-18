4b198efd18b015f5147e2f3614f9a41b212560d5
abureau <abureau6@gmail.com>
2024-11-18 12:10:17 -0500

    Correcting TADs + adding pathway

--- final_freeze/code/bootstrap/bootstrap_final_freeze.R
+++ final_freeze/code/bootstrap/bootstrap_final_freeze.R
@@ -1,6 +1,9 @@
 # Code for recomputing with the bootstrap the p-values of the best signal of the CRH analysis
 library(stringr); library(GenomicRanges); library(RetroFunRVS); library(dplyr)
 
+source("extrait_pathway.R")
+source("extrait_CRH_TAD.R")
+
 path_retrofun <- "/lustre03/project/6033529/quebec_10x/data/WGS_bs_2022/500_samples_cag/RetroFunRVS"
 #setwd(paste0(path_retrofun, "/TADs"))
 
@@ -45,28 +48,52 @@ CRH1006.agg = extract_CRH(TAD.chr21.1.orig,"CRH1006")
 
 # Avec relations cryptiques
 TAD.chr21.1.cryptic = agg.genos.annotations.TAD(pheno="GCbr", with_exons=TRUE, consanguinity=TRUE, path_retrofun=path_retrofun, chr=21, TAD=1, maxvar = 300)
-null_name <- "consanguinity.cryptique.seq.prob.dist.rds"
-null <- readRDS(paste0(path_retrofun, "/objets_ped/", null_name))
+null_cryptic_name <- "consanguinity.cryptique.seq.prob.dist.rds"
+null.cryptic <- readRDS(paste0(path_retrofun, "/objets_ped/", null_name))
 exp_var_cryptic_name <- "expected.variance.consanguinity.cryptique.seq.rds"
 exp_var.cryptic <- readRDS(paste0(path_retrofun, "/objets_ped/", exp_var_cryptic_name))
 
 results <- RetroFun.RVS(exp_var_cryptic, TAD.chr21.1.cryptic$agg.genos.by.fam, Z_annot = TAD.chr21.1.cryptic$Z_annot, W = rep(1, nrow(TAD.chr21.1.cryptic$Z_annot)), independence=FALSE)
 
 set.seed(10)
-boot.TAD.chr21.1.cryptic = bootRetroFunRVS(TAD.chr21.1.cryptic$agg.genos.by.fam, n.unique.config.by.fam=null$N, Z_annot = TAD.chr21.1.cryptic$Z_annot[,-4], prob.sharing.by.fam=null$pattern.prob,nullval=exp_var,nrep=1000)
-Var.Stat = unlist(compute.Var.by.Annot(exp_var,TAD.chr21.1.cryptic$agg.genos.by.fam,TAD.chr21.1.cryptic$Z_annot,W= rep(1, nrow(TAD.chr21.1.cryptic$Z_annot)),independence = FALSE))
+boot.TAD.chr21.1.cryptic = bootRetroFunRVS(TAD.chr21.1.cryptic$agg.genos.by.fam, n.unique.config.by.fam=null.cryptic$N, Z_annot = TAD.chr21.1.cryptic$Z_annot[,-4], prob.sharing.by.fam=null.cryptic$pattern.prob,nullval=exp_var.cryptic,nrep=1000)
+Var.Stat = unlist(compute.Var.by.Annot(exp_var.cryptic,TAD.chr21.1.cryptic$agg.genos.by.fam,TAD.chr21.1.cryptic$Z_annot,W= rep(1, nrow(TAD.chr21.1.cryptic$Z_annot)),independence = FALSE))
 
 pmat = pnorm(boot.TAD.chr21.1.cryptic/matrix(sqrt(Var.Stat[-4]),1000,3,byrow = T),0,1,lower.tail = FALSE)
 
 boot.TAD.chr21.1.cryptic.ACAT = apply(pmat,1,function(x) ACAT::ACAT(x[!is.nan(x)]))
 mean(boot.TAD.chr21.1.cryptic.ACAT<=results$ACAT)
-# [1] 0.008
+# [1] 0.001
 
 set.seed(10)
 CRH998.cryptic.agg = extract_CRH(TAD.chr21.1.cryptic,"CRH998")
-CRH998.cryptic.boot = bootRetroFunRVS(CRH998.cryptic.agg, n.unique.config.by.fam=null$N, prob.sharing.by.fam=null$pattern.prob,nullval=exp_var.cryptic,nrep=10000)
+CRH998.cryptic.boot = bootRetroFunRVS(CRH998.cryptic.agg, n.unique.config.by.fam=null.cryptic$N, prob.sharing.by.fam=null.cryptic$pattern.prob,nullval=exp_var.cryptic,nrep=10000)
 
 annotation.matrix = matrix(1, ncol=1, nrow=max(CRH998.agg$index_variants))
 score_cryptic.CRH998 = compute.Burden.by.Annot(exp_var, CRH998.cryptic.agg,  Z_annot = annotation.matrix, W = rep(1, nrow(annotation.matrix)))$B[1,1]
 mean(CRH998.cryptic.boot>=score_cryptic.CRH998)
 #[1] 0.0019
+
+# Code for recomputing with the bootstrap the p-values of the best signal of the pathway analysis
+
+# Sans relations cryptiques
+GO.0098939.orig = agg.genos.pathways(pheno="GCbr", with_exons=TRUE, consanguinity=FALSE, path_retrofun=path_retrofun, id="GO:0098939")
+set.seed(11)
+GO.0098939.boot = bootRetroFunRVS(GO.0098939.orig, n.unique.config.by.fam=null$N, prob.sharing.by.fam=null$pattern.prob,nullval=exp_var,nrep=10000)
+
+annotation.matrix = matrix(1, ncol=1, nrow=max(GO.0098939.orig$index_variants))
+score_orig.GO.0098939 = compute.Burden.by.Annot(exp_var, GO.0098939.orig,  Z_annot = annotation.matrix, W = rep(1, nrow(annotation.matrix)))$B[1,1]
+
+mean(GO.0098939.boot>=score_orig.GO.0098939)
+# [1] 7e-04
+
+# Avec relations cryptiques
+GO.0098939.cryptic = agg.genos.pathways(pheno="GCbr", with_exons=TRUE, consanguinity=TRUE, path_retrofun=path_retrofun, id="GO:0098939")
+set.seed(11)
+GO.0098939.cryptic.boot = bootRetroFunRVS(GO.0098939.cryptic, n.unique.config.by.fam=null.cryptic$N, prob.sharing.by.fam=null.cryptic$pattern.prob,nullval=exp_var.cryptic,nrep=10000)
+
+annotation.matrix = matrix(1, ncol=1, nrow=max(GO.0098939.cryptic$index_variants))
+score_cryptic.GO.0098939 = compute.Burden.by.Annot(exp_var.cryptic, GO.0098939.cryptic,  Z_annot = annotation.matrix, W = rep(1, nrow(annotation.matrix)))$B[1,1]
+
+mean(GO.0098939.cryptic.boot>=score_cryptic.GO.0098939)
+#[1] 7e-04
